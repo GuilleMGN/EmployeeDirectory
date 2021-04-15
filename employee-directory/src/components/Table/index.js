051a0cfb160b8123c7c17cf64fa8824241e0b4bf
Matthew Guillen <matthewguillen777@gmail.com>
2021-04-14 21:17:22 -0400

    add sort by age fn

--- employee-directory/src/components/Table/index.js
+++ employee-directory/src/components/Table/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Table.css";
 
 
-function Table(props, sortBy) {
+function Table(props) {
     return (
         <table className="table table-striped table-hover table-condensed table-sortable">
             <thead>
@@ -10,26 +10,30 @@ function Table(props, sortBy) {
                     <th>Image</th>
                     <th data-field="name" data-sortable="true">Name <span> </span>
                         <button onClick={() => props.sortBy("name", "first", "last")}
-                          className="btn-outline-secondary"> ⇅
+                            className="btn-outline-secondary"> ⇅
                         </button>
                     </th>
                     <th>Phone</th>
                     <th>Email</th>
-                    <th>Age</th>
+                    <th data-field="age" data-sortable="true">Age <span> </span>
+                        <button onClick={() => props.sortBy("dob", "date", "age")}
+                            className="btn-outline-secondary"> ⇅
+                        </button>
+                    </th>
                 </tr>
             </thead>
             <tbody>
                 {(props.users.length > 0) ? props.users.map((data, index) => {
                     return (
                         <tr key={index}>
-                            <td><img src={data.picture.medium} alt={data.name.first}/></td>
+                            <td><img src={data.picture.medium} alt={data.name.first} /></td>
                             <td>{data.name.first} {data.name.last}</td>
                             <td>{data.cell}</td>
                             <td>{data.email}</td>
                             <td>{data.dob.age}</td>
                         </tr>
                     )
-                }) : <tr><td colSpan="5">Loading...</td></tr>}
+                }) : <tr><td colSpan="5">No results found. </td></tr>}
             </tbody>
         </table>
     );
